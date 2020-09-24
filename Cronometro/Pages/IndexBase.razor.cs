971cf8163f4f50bb3e1ed06d91d63c0cad9e7cd8
percy luis leon lucas <percy10_leon@hotmail.com>
2020-09-24 17:03:22 -0500

    ejemplo

---
+++ Cronometro/Pages/IndexBase.razor.cs
@@ -0,0 +1,38 @@
+﻿using Cronometro.Utilitario;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Cronometro.Pages
+{
+    public class IndexBase:ComponentBase
+    {
+        protected string tiempoAtencion = "00:00:00";
+        protected Timer timerAtencion;
+        protected Timer timerOperacion = new Timer();
+        protected XXCronometro cronometroAtencion = new XXCronometro();
+
+        private void TimerOnAtencion(object sender, ElapsedEventArgs e)
+        {
+            tiempoAtencion = cronometroAtencion.ShowTimer();
+            StateHasChanged();
+        }
+
+        protected async Task Iniciar() 
+        {
+            timerAtencion = new Timer();
+            timerAtencion.Interval = 1000;
+            timerAtencion.Elapsed += TimerOnAtencion;
+            timerAtencion.Start();
+        }
+
+
+        protected async Task Terminar()
+        {
+            timerAtencion.Close();
+            cronometroAtencion.Inicializar();
+        }
+
+        
+    }
+}
---
+++ Cronometro/Utilitario/XXCronometro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cronometro.Utilitario
+{
+    public class XXCronometro
+    {
+        protected int h = 0;
+        protected int m = 0;
+        protected int s = 0;
+
+        public string ShowTimer()
+        {
+            var segundos = "";
+            var minutos = "";
+            var horas = "";
+
+            s++;
+
+            if (s > 59) { m++; s = 0; }
+            if (m > 59) { h++; m = 0; }
+            if (h > 24) { h = 0; }
+
+            segundos = (s < 10) ? ("0" + s.ToString()) : (s.ToString());
+            minutos = (m < 10) ? ("0" + m.ToString()) : (m.ToString());
+            horas = (h < 10) ? ("0" + h.ToString()) : (h.ToString());
+
+            return horas + ":" + minutos + ":" + segundos;
+        }
+
+        public void Inicializar()
+        {
+            h = 0;
+            m = 0;
+            s = 0;
+        }
+    }
+}
